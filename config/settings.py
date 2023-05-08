8621598986720629d87a834aa58277036995e281
Carlos Escutia <carlosescutia@gmail.com>
2023-05-08 15:09:31 -0600

    Se agrega app infomun.

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "infomun",
 ]
 
 MIDDLEWARE = [
