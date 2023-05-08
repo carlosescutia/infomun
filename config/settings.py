088d398d2e297649086ae7d14730722ed43c3584
Carlos Escutia <carlosescutia@gmail.com>
2023-05-08 15:18:31 -0600

    Se agrega carpeta mediafiles.

--- config/settings.py
+++ config/settings.py
@@ -126,9 +126,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# Global static files
+STATICFILES_DIRS = [
+    BASE_DIR / "globalstatic"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+# Trusted origins
+CSRF_TRUSTED_ORIGINS = ["https://iplaneg.guanajuato.gob.mx"]
