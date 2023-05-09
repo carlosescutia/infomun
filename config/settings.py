f6d60f93194272c91c2232c18f6a4b7d5a7d04ff
Carlos Escutia <carlosescutia@gmail.com>
2023-05-09 15:09:11 -0600

    Se ajusta configuración de static, mediafiles.

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Global static files
 STATICFILES_DIRS = [
-    BASE_DIR / "globalstatic"
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
